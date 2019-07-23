bf586be6304b29958dfa1a57f365398a78192fad
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-23 13:46:13 -0300

    Adding project to repository.

---
+++ src/test/java/com/udemy/springcloudconfigserver/springCloudConfigServer/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springcloudconfigserver.springCloudConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
